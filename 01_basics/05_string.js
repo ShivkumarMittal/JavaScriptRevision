bc4a0455d7994693b9059fbbd00b72e5069203bc
Shivkumar Mittal <shivkumarmittal457@gmail.com>
2024-07-25 13:20:35 +0530

    number

---
+++ 01_basics/05_string.js
@@ -0,0 +1,39 @@
+// const name = "shiv"
+// const repoCount = 50
+
+// console.log(name[0])
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name)
+// console.log(typeof(name))
+
+// console.log(name+repoCount+12)
+
+// console.log(`Hello my name is ${name} and ,y repo is ${repoCount}`);
+
+const gameName = new String("Shivkjasjfakjdfas");
+// console.log(gameName[0])
+// console.log(gameName)
+// console.log(gameName.toUpperCase())
+// console.log(gameName)
+// console.log(typeof(gameName))
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('i'));
+
+// const newString = gameName.substring(0,2)
+// console.log(newString)
+
+
+// const anotherString = gameName.slice(-1,5)
+// console.log(anotherString)
+
+// const newStringOne = "   shiv    "
+// console.log(newStringOne.trim());
+
+// const url = "https://localhost//hite%20sh25"
+// url.replace('%20','-')
+// console.log(url.replace('%20','-'));
+
+const name = "shiv is a js programmer"
+console.log(name.split(' '))
---
+++ 01_basics/06_number.js
@@ -0,0 +1,31 @@
+
+// const score = 45
+
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(typeof balance)
+
+// // console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 1223.5689
+
+// console.log(otherNumber.toPrecision(4))
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//...................................
+//Math
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(4.2));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(4,2,3))
+
+// console.log((Math.random()*10))
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
