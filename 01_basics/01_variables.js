75a7ec62a6c4f6c538174b6511cc82d92e3a17cb
Shivkumar Mittal <shivkumarmittal457@gmail.com>
2024-07-25 09:06:00 +0530

    tii datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId  = 141425
+let accountEmail = "shivkumarmittal145@gmial.com"
+var accountPassword = "123650"
+accountcity = 5
+let accountState 
+
+//accountId = 2
+accountEmail = "shiv@gamil.com"
+accountPassword = "0000"
+
+console.log(typeof(accountEmail));
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
+console.table([typeof(accountId),typeof(accountEmail),typeof(accountPassword),typeof(accountcity),typeof(accountState)])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello"); //we are using nodejs , not browser
+
+console.log(3+3)
+console.log("hitesh")
+
+let name = "shiv";
+let age = 18
+let isLoggedin = false
+let data = null
+let length
+
+//number 
+//bigint
+//string
+//boolean
+//null => standalone value hai type object hota hai
+//undefined => value assign nahi hui hai
+//symbol => unique ke liye
+
+
+//object
+
+console.log(typeof(data));
+console.log(typeof(length));
