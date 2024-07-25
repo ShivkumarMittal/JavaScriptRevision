83b0a23c21fb18872295b98c59827edefdead61e
Shivkumar Mittal <shivkumarmittal457@gmail.com>
2024-07-25 09:26:07 +0530

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let age = false
+let score = 32
+
+let valueInNumber = Number(age)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33adf0" => NaN
+
+let isLoggedin = "shiv"
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
